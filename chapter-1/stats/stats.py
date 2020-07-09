a7de818689afb4b98e397c48888b2505ecdae570
Yun Zhang <jslgdxzy@gmail.com>
2020-07-09 23:32:16 +0930

    reformat the code

--- chapter-1/stats/stats.py
+++ chapter-1/stats/stats.py
@@ -1,8 +1,10 @@
 # stats.py
 
+
 def init():
     global _stats
     _stats = {}
+
 
 def event_occurred(event):
     global _stats
@@ -10,6 +12,7 @@ def event_occurred(event):
         _stats[event] = _stats[event] + 1
     except KeyError:
         _stats[event] = 1
+
 
 def get_stats():
     global _stats
